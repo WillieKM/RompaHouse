83e1ced8b83dbe199ee1109ea1a33c075d9bb8a6
WillieKM <willie@cyber-node.com>
2026-05-05 21:05:32 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <Header />
       <HeroSection />
       <WelcomeSection />
+      <MissionSection />
       <ServicesShowcase />
       <TestimonialsSection />
       <FaqSection />
